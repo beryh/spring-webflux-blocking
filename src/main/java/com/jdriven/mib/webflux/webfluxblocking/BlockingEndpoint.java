e55bf313ad53c9c1cb3b100fefcf20b56e4a261c
michel.breevoort <michel.breevoort@columbo.nl>
2020-10-21 17:39:48 +0200

    initial code

---
+++ src/main/java/com/jdriven/mib/webflux/webfluxblocking/BlockingEndpoint.java
@@ -0,0 +1,38 @@
+package com.jdriven.mib.webflux.webfluxblocking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController()
+@RequestMapping("block")
+public class BlockingEndpoint {
+    private final static Logger LOGGER = LoggerFactory.getLogger(BlockingEndpoint.class);
+
+    @GetMapping(value = "time/{sleepMs}", produces = MediaType.TEXT_PLAIN_VALUE)
+    public Mono<String> block(@PathVariable long sleepMs) {
+        return Mono.fromSupplier(() -> blockingFunction(sleepMs));
+        // enable for non blocking: .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    private String blockingFunction(long sleepMs) {
+        LOGGER.info("Start sleep for " + sleepMs);
+        try {
+            Thread.sleep(sleepMs);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        LOGGER.info("End sleep for " + sleepMs);
+        return "OK, wake up from " + sleepMs + " ms sleep";
+    }
+
+    @GetMapping(value = "health", produces = MediaType.TEXT_PLAIN_VALUE)
+    public String health() {
+        return "OK";
+    }
+}
